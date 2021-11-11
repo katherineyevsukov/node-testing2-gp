28647900acf7df4c18bd1d6e1e46414137f68322
Kat <kyevsukov@gmail.com>
2021-11-11 13:10:22 -0500

    wrote first test

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -17,3 +17,20 @@ beforeEach(async () => {
 afterAll(async () => {
   await db.destroy()
 })
+
+describe('Hobbit model', /* NO ASYNC HERE!!! */ () => {
+  describe('getAll()', () => {
+    test('resolves all hobbits in the db', async () => {
+      const data = await Hobbit.getAll()
+      expect(data.length).toBe(4)
+      expect(data).toHaveLength(4)
+    })
+    test.todo('resolves the correct shapes')
+  })
+  describe('getById()', () => {
+
+  })
+  describe('insert()', () => {
+
+  })
+})
