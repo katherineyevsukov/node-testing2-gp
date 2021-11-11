7be58da918f1a27537aa3213f4a260165ccec8f0
Kat <kyevsukov@gmail.com>
2021-11-11 13:28:40 -0500

    added lifescycles totest

--- api/server.test.js
+++ api/server.test.js
@@ -1,3 +1,17 @@
 const server = require('./server')
 const request = require('supertest')
 const db = require('../data/dbConfig')
+
+
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+  })
+  
+  beforeEach(async () => {
+    await db.seed.run()
+  })
+  
+  afterAll(async () => {
+    await db.destroy()
+  })
