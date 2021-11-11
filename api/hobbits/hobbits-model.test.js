daf44bf6b754c95e4a7ee09b470ef634dec4bcc6
Kat <kyevsukov@gmail.com>
2021-11-11 12:52:22 -0500

    set up lifecylce hooks

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,3 +1,19 @@
+const Hobbit = require('./hobbits-model')
+const db = require('../../data/dbConfig')
+
 test('is testing environment', () => {
-    expect(process.env.NODE_ENV).toBe('testing')
-  })
+  expect(process.env.NODE_ENV).toBe('testing')
+})
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async () => {
+  await db.seed.run()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
