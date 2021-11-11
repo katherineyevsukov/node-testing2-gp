5afda997a2a4827018e1f661a50c9980135896e6
Kat <kyevsukov@gmail.com>
2021-11-11 13:25:49 -0500

    wrote second model test

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,5 +1,6 @@
 const Hobbit = require('./hobbits-model')
 const db = require('../../data/dbConfig')
+const { hobbits } = require('../../data/seeds/001-hobbits')
 
 test('is testing environment', () => {
   expect(process.env.NODE_ENV).toBe('testing')
@@ -18,17 +19,45 @@ afterAll(async () => {
   await db.destroy()
 })
 
-describe('Hobbit model', /* NO ASYNC HERE!!! */ () => {
+describe('Hobbit model', /* NO ASYNC HERE!!! */() => {
   describe('getAll()', () => {
+    let data
+    beforeEach(async () => {
+      data = await Hobbit.getAll()
+    })
     test('resolves all hobbits in the db', async () => {
-      const data = await Hobbit.getAll()
       expect(data.length).toBe(4)
       expect(data).toHaveLength(4)
     })
-    test.todo('resolves the correct shapes')
+    test('resolves the correct shapes', async () => {
+      expect(data).toMatchObject(hobbits)
+      expect(data).toEqual([
+        {
+          "id": 1,
+          "name": "sam"
+        },
+        {
+          "id": 2,
+          "name": "frodo"
+        },
+        {
+          "id": 3,
+          "name": "pippin"
+        },
+        {
+          "id": 4,
+          "name": "merry"
+        }
+      ])
+    })
   })
   describe('getById()', () => {
-
+    test('returns the correct hobbit', async () => {
+      // hit the function, get hobbit with id = 1
+      // assert that the data is { id: 1, name: 'sam' }
+      const data = await Hobbit.getById('1')
+      expect(data).toMatchObject({ id: 1, name: 'sam' })
+    })
   })
   describe('insert()', () => {
 
